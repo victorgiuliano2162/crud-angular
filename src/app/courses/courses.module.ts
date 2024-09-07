e484a3f377a8da7b175820d53a70482354f2e5e1
Victor Giuliano <143557298+victorgiuliano2162@users.noreply.github.com>
2024-09-07 14:47:17 -0300

    component alteration

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -4,14 +4,16 @@ import { CommonModule } from '@angular/common';
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
 
+import { AppMaterialModule } from '../shared/app-material/app-material.module';
 
 @NgModule({
   declarations: [
-    CoursesComponent
+    CoursesComponent,
   ],
   imports: [
+    AppMaterialModule,
+    CoursesRoutingModule,
     CommonModule,
-    CoursesRoutingModule
   ]
 })
 export class CoursesModule { }
--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
+import { CoursesService } from '../services/courses.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-courses',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoursesComponent implements OnInit {
 
-  constructor() { }
+  courses: Observable<Course[]>;
+  displayedColumns = ['name', 'category'];
+
+  //coursesService: CoursesService;
+
+
+  constructor(private coursesService: CoursesService) {
+    //this.courses = [];
+    //é possível inicializar a variável tanto no construtor quanto no ngOnInit ou na declaração e não há diferença
+    //this.coursesService = new CoursesService();
+    this.courses = this.coursesService.list();
+  }
 
   ngOnInit(): void {
+    //renderiza somente quando o componente é renderizado
+    this.courses = this.coursesService.list();
   }
 
 }
