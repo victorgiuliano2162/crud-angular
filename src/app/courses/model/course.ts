b898325ce739431b6b97594a61958c34401c0e2c
Victor Giuliano <143557298+victorgiuliano2162@users.noreply.github.com>
2024-09-07 14:47:27 -0300

    component interface

---
+++ src/app/courses/model/course.ts
@@ -0,0 +1,8 @@
+export interface Course {
+  _id: string;
+  name: string;
+  category: string;
+
+}
+
+//it only exists in developing stage to help intelli sense and developer, later this will replaced
