171904fadc16ed8aa70d2da8ca41b2370a309ce6
Victor Giuliano <143557298+victorgiuliano2162@users.noreply.github.com>
2024-09-08 21:30:32 -0300

    error handling with matdialog for loading errors

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -3,38 +3,46 @@ import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent implements OnInit {
-
   courses$: Observable<Course[]>;
   //dolar sign indica
   displayedColumns = ['name', 'category'];
 
   //coursesService: CoursesService;
 
-
-  constructor(private coursesService: CoursesService) {
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog
+  ) {
     //this.courses = [];
     //é possível inicializar a variável tanto no construtor quanto no ngOnInit ou na declaração e não há diferença
     //this.coursesService = new CoursesService();
-    this.courses$ = this.coursesService.list()
-    .pipe(
-      catchError(error => {
-        console.log(error)
-        return of([])
+    this.courses$ = this.coursesService.list().pipe(
+      catchError((error) => {
+        this.onError('Erro ao carregar a lista de cursos')
+        console.log(error);
+        return of([]);
       })
       //catchError must return an observable
       //the 'of' automatic create an observable
     );
   }
 
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg,
+    });
+  }
+
   ngOnInit(): void {
     //renderiza somente quando o componente é renderizado
   }
-
 }
