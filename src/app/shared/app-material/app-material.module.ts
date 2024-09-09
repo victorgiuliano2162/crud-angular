ec0c9da7317bc32503222e5d1a3fcc64b22e537b
Victor Giuliano <143557298+victorgiuliano2162@users.noreply.github.com>
2024-09-08 21:29:07 -0300

    added mat dialog, button and icon module

--- src/app/shared/app-material/app-material.module.ts
+++ src/app/shared/app-material/app-material.module.ts
@@ -3,6 +3,10 @@ import { MatTableModule } from '@angular/material/table';
 import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @NgModule({
   exports: [
@@ -10,6 +14,9 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
     MatTableModule,
     MatToolbarModule,
     MatProgressSpinnerModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatIconModule,
   ],
 })
 export class AppMaterialModule { }
