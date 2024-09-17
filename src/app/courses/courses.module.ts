ae3f79b6487fc8fe714491dc137c197cf49555d7
Victor Giuliano <143557298+victorgiuliano2162@users.noreply.github.com>
2024-09-17 08:54:54 -0300

    add new route

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -6,10 +6,12 @@ import { CoursesComponent } from './courses/courses.component';
 
 import { AppMaterialModule } from '../shared/app-material/app-material.module';
 import { SharedModule } from '../shared/shared.module';
+import { CourseFormComponent } from './course-form/course-form.component';
 
 @NgModule({
   declarations: [
     CoursesComponent,
+    CourseFormComponent,
   ],
   imports: [
     AppMaterialModule,
