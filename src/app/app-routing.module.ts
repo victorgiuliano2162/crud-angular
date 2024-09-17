8625654064d85d9d817e1b49331f5defedc63309
Victor Giuliano <143557298+victorgiuliano2162@users.noreply.github.com>
2024-09-17 08:56:47 -0300

    edit routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CourseFormComponent } from './courses/course-form/course-form.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'courses' },
-  { path: 'courses',
-    loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule)
-    }
+  {
+    path: 'courses',
+    loadChildren: () =>
+      import('./courses/courses.module').then((m) => m.CoursesModule),
+  },
+  //{
+    //path: 'new',
+    //component: CourseFormComponent,
+  //},
 ];
 
 @NgModule({
