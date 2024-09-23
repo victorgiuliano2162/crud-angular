d08b6999bd08626b7a5f4f5aab4fd1ca7c99970e
Victor Giuliano <143557298+victorgiuliano2162@users.noreply.github.com>
2024-09-23 10:28:30 -0300

    feedbacks for post method

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -3,41 +3,50 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { CoursesService } from '../services/courses.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-course-form',
   templateUrl: './course-form.component.html',
-  styleUrls: ['./course-form.component.scss']
+  styleUrls: ['./course-form.component.scss'],
 })
 export class CourseFormComponent implements OnInit {
-
   form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder,
+  constructor(
+    private formBuilder: FormBuilder,
     private service: CoursesService,
     private router: Router,
-    private snackBar: MatSnackBar
+    private snackBar: MatSnackBar,
+    private localtion: Location
   ) {
     this.form = this.formBuilder.group({
-      name:[null],
+      name: [null],
       category: [null],
     });
-   }
-
-  ngOnInit(): void {
   }
 
+  ngOnInit(): void {}
+
   onSubmit() {
-    this.service.save(this.form.value)
-    .subscribe(data => console.log(data), error => {
-      this.snackBar.open("Erro ao salvar curso","Fechar" , { duration: 2500})
-    });
+    this.service.save(this.form.value).subscribe(
+      (result) => this.onSucess(),
+      (error) => this.onError()
+    );
     //console.log(this.form.value);
   }
 
   onCancel() {
-    console.log("Concelou course form");
-    this.router.navigate([''])
+    console.log('Concelou course form');
+    this.localtion.back();
+    }
+
+  private onError() {
+    this.snackBar.open('Erro ao salvar curso', 'Fechar', { duration: 2500 });
   }
 
+  private onSucess() {
+    this.snackBar.open('Curso salvo com sucesso', '', { duration: 2500 });
+    this.onCancel();
+  }
 }
