eb60f0176471fa52643d399a5d79af4a7375e829
Victor Giuliano <143557298+victorgiuliano2162@users.noreply.github.com>
2024-09-23 19:51:07 -0300

    methods for create, update and improvment save

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -23,17 +23,31 @@ export class CoursesService {
       first(), //obtém a primeira resposta do servidor e finalizar a incrição
       //take(1), consulta um determinado número de vezes
       delay(500), //o valor é passado em ms
-      tap(courses => console.log(courses))
+      tap((courses) => console.log(courses))
     );
     //o pipe habilita a manipulação da resposta
   }
 
   loadById(id: string) {
-    return this.httpClient.get<Course>(`${this.API}/${id}`)
+    return this.httpClient.get<Course>(`${this.API}/${id}`);
   }
 
-    save(record: Partial<Course>) {
-      return this.httpClient.post<Course>(this.API, record).pipe(first());
+  save(record: Partial<Course>) {
+    if (record._id) {
+      return this.update(record);
     }
-    //Partial tag allow few fields of a object
+    console.log("creou")
+    return this.create(record);
+  }
+  //Partial tag allow few fields of a object
+
+  private create(record: Partial<Course>) {
+    return this.httpClient.post<Course>(this.API, record).pipe(first());
+  }
+
+  private update(record: Partial<Course>) {
+    return this.httpClient
+      .put<Course>(`${this.API}/${record._id}`, record)
+      .pipe(first());
+  }
 }
